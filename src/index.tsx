b8fcff548ed929425fe650a0005c0f5e56772ad3
ansi0545 <ann-sofie.simonsson-haasz.0545@student.uu.se>
2024-11-24 12:15:38 +0100

    Update dependencies and TypeScript configuration: add type definitions for React and React Router, upgrade TypeScript, and adjust paths in tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
